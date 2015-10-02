f18f9fe22b326372654f86af2a3d293e9dc2bc32
fumingivy <mm.fu@mail.utoronto.ca>
2015-10-02 15:13:47 -0400

    Lab 1 - Ming Fu

--- lab1.py
+++ lab1.py
@@ -2,10 +2,8 @@
 
 """ Graded Lab #1 for Inf1340, Fall 2015 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-__copyright__ = "2015 Susan Sim"
-__license__ = "MIT License"
+__author__ = 'Ming Fu'
+__email__ = "mm.fu@mail.utoronto.ca"
 
 
 def vowel_or_consonant():
